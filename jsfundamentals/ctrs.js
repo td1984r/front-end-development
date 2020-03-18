e82c575460adc81da88de1d74ed10a54dd4cb0c8
td1984r <39582585+td1984r@users.noreply.github.com>
2020-03-18 16:41:35 +0100

    Update ctrs.js

--- jsfundamentals/ctrs.js
+++ jsfundamentals/ctrs.js
@@ -1,6 +1,7 @@
     var pPrecnikValjka = prompt("Molim Vas unesite poluprecnik valjka:");
     var visinaValjka = prompt("Molim Vas unesite visinu valjka:");
+    var pPrecnikValjkaNaKvadrat = pPrecnikValjkae2;
     const PI = "3,14"
 
-    var zapValjka = parseInt(pPrecnikValjka)e2 * PI * parseInt(visinaValjka);
+    var zapValjka = parseInt(pPrecnikValjkaNaKvadrat) * PI * parseInt(visinaValjka);
     alert("Zapremina valjka je: " + zapValjka);
